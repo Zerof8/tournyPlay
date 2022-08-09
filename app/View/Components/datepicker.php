55c567d1da624d66fc11bf42314119fe7f79a5c3
Abdalrahman <abdalrahman.ghname@gmail.com>
2022-08-09 04:06:06 +0200

    Fix an error with the datepicker.php component

--- app/View/Components/datepicker.php
+++ app/View/Components/datepicker.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class date-picker extends Component
+class datepicker extends Component
 {
     /**
      * Create a new component instance.
