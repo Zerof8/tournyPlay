ab67891aade81b85b94f312b10a2bbbc48b8c9a6
Abdalrahman <abdalrahman.ghname@gmail.com>
2022-08-28 21:13:58 +0200

    Created subscription page + Address component

---
+++ resources/views/livewire/subscriptions.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Subscription') }}
+        </h2>
+    </x-slot>
+
+    <div>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            @if(Auth::user()->address == null)
+            <livewire:update-address-form/>
+            @endif
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/livewire/update-address-form.blade.php
@@ -0,0 +1,51 @@
+<x-jet-form-section submit="address">
+    <x-slot name="title">
+        {{ __('Address') }}
+    </x-slot>
+
+    <x-slot name="description">
+        {{ __('Enter your billing address in order to buy credits or subscribe!') }}
+    </x-slot>
+
+    <x-slot name="form">
+        <!-- Address -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-jet-label for="address" value="{{ __('Address') }}" />
+            <x-jet-input type="text" class="w-full" id="address" wire:model.defer="state.address" />
+            <x-jet-input-error for="address" class="mt-2" />
+        </div>
+        <!-- City -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-jet-label for="city" value="{{ __('City') }}" />
+            <x-jet-input type="text" class="w-full" id="city" wire:model.defer="state.city" />
+            <x-jet-input-error for="city" class="mt-2" />
+        </div>
+        <!-- State -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-jet-label for="state" value="{{ __('State') }}" />
+            <x-jet-input type="text" class="w-full" id="state" wire:model.defer="state.state" />
+            <x-jet-input-error for="state" class="mt-2" />
+        </div>
+        <!-- Zip -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-jet-label for="zip" value="{{ __('Zip') }}" />
+            <x-jet-input type="text" class="w-full" id="zip" wire:model.defer="state.zip" />
+            <x-jet-input-error for="zip" class="mt-2" />
+        </div>
+        <!-- Country -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-jet-label for="country" value="{{ __('Country') }}" />
+            <x-jet-input type="text" class="w-full" id="country" wire:model.defer="state.country" />
+            <x-jet-input-error for="country" class="mt-2" />
+        </div>
+    </x-slot>
+        <x-slot name="actions">
+            <x-jet-action-message class="mr-3" on="saved">
+                {{ __('Saved.') }}
+            </x-jet-action-message>
+
+            <x-jet-button wire:loading.attr="disabled" wire:target="save">
+                {{ __('Save') }}
+            </x-jet-button>
+        </x-slot>
+</x-jet-form-section>
