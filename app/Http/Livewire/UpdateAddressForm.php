3000c486445230baf3b00d162d3f1786937dd247
Abdalrahman <abdalrahman.ghname@gmail.com>
2022-08-29 20:54:05 +0200

    Applied referrer logic to the updating method

--- app/Http/Livewire/UpdateAddressForm.php
+++ app/Http/Livewire/UpdateAddressForm.php
@@ -22,7 +22,13 @@ class UpdateAddressForm extends Component
         'address' => 'required',
         'state' => 'required',
     ];
-
+    protected $messages = [
+        'country' => 'Please enter a valid country name', //!!!!!!!!!!!
+        'city' =>'Please enter a valid city name',
+        'zip' => 'Please enter a valid zip code',
+        'address' => 'Please enter your valid street name and house number',
+        'state' => 'Please enter a valid state name',
+    ];
     public function save()
     {
         //validate and store the form inside the array
@@ -38,15 +44,15 @@ class UpdateAddressForm extends Component
         $currentUser = User::find($userId);
         $currentUser->address = $this->addressArray;
         $currentUser->save();
-
-        if(route('subscription'))
-        {
-            return redirect()->route('subscription');
+        if (request()->header('Referer') == route('subscription')) {
+            return redirect(request()->header('Referer'));
         }
     }
+
 
     public function render()
     {
         return view('livewire.update-address-form');
     }
+
 }
