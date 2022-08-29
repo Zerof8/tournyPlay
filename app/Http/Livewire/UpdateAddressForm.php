624f0a56547f8c34796bed6711bdf6e03f27700a
Abdalrahman <abdalrahman.ghname@gmail.com>
2022-08-29 14:43:24 +0200

    Address test

--- app/Http/Livewire/UpdateAddressForm.php
+++ app/Http/Livewire/UpdateAddressForm.php
@@ -2,20 +2,39 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Validation\ValidationException;
 use Livewire\Component;
 
 class UpdateAddressForm extends Component
 {
     public $country;
+    public $city;
+    public $zip;
+    public $address;
+    public $state;
+    public $addressArray;
 
-    public function validate($rules = null, $messages = [], $attributes = [])
-    {
-
-    }
+    protected $rules = [
+        'country' => 'required',
+        'city' => 'required',
+        'zip' => 'required',
+        'address' => 'required',
+        'state' => 'required',
+    ];
 
     public function save()
     {
-        return auth()->user()->address;
+       $this->validate();
+        $this->emit('addressSaved');
+        $this->addressArray = [
+            'country' => $this->country,
+            'city' => $this->city,
+            'zip' => $this->zip,
+            'address' => $this->address,
+            'state' => $this->state,
+        ];
+        $user = auth()->user();
+        $user->address = $this->addressArray;
     }
 
     public function render()
